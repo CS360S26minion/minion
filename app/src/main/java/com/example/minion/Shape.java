4bdfe347b5735cb250dc2e177e84dbbd699a82f8
Shaheer-Ahmd <shaheerahmad57575@gmail.com>
2026-02-17 13:26:44 +0500

    added original color

--- app/src/main/java/com/example/minion/Shape.java
+++ app/src/main/java/com/example/minion/Shape.java
@@ -4,6 +4,7 @@ public abstract class Shape {
 
     protected int x;
     protected int y;
+    public String color = "Red [Original Color]";
 
     public Shape(int x, int y) {
         this.x = x;
